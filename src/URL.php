adc6e5da0a799c45c774d0bfd6f8e978440ad5f5
Joby Elliott <joby@byjoby.com>
2020-02-01 21:10:38 -0700

    URL bug fix

--- src/URL.php
+++ src/URL.php
@@ -46,6 +46,10 @@ class URL
             // set port
             if (@$parsed['port']) {
                 $this->setPort($parsed['port']);
+            } elseif ($this->scheme() == 'https') {
+                $this->setPort(443);
+            } else {
+                $this->setPort(80);
             }
             // path, query, fragment are now straightforward
             $this->setPath(@$parsed['path'] ?? '/');
@@ -390,6 +394,7 @@ class URL
             if ($site = URLFactory::site()) {
                 return $site->port();
             } else {
+                return 80;
                 throw new Exception("No site to get port from");
             }
         }
