4209f89c3ff38e0714bbc1830dfb6dfab6904456
Joby Elliott <code@byjoby.com>
2020-05-02 14:40:42 -0600

    plugin fix

--- src/Composer/Plugin.php
+++ src/Composer/Plugin.php
@@ -2,12 +2,21 @@
 namespace Leafcutter\Composer;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\EventDispatcher\EventSubscriberInterface;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
+    protected $composer;
+    protected $io;
+
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+    }
+
     public static function onPostAutoloadDump($event)
     {
         var_dump('autoload event?');
@@ -16,7 +25,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            'post-autoload-dump' => 'postAutoloadDump'
+            'post-autoload-dump' => 'postAutoloadDump',
         );
     }
 }
